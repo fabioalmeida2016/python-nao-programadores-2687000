0b7a78442e1ba9f13b324d59c2d32813efa7d0a7
fabioalmeida2016 <107158133+fabioalmeida2016@users.noreply.github.com>
2025-11-26 09:50:02 +0000

    execução

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+cao = 'Bianca'
+cao
 # Declare variáveis de cada tipo numérico
-
+Altura = 1.69
+print(Altura)
+Peso = 62
+print(Peso)
 
 # Declare uma variável do tipo booleano
+Vida_eterna = True
+print(Vida_eterna)
+Reencarnação = False
+print(Reencarnação)
