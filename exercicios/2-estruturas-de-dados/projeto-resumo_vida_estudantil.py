e36d33c940a2282d285728247d4476b70c39f052
fabioalmeida2016 <107158133+fabioalmeida2016@users.noreply.github.com>
2025-11-26 23:24:28 +0000

    resolução

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,16 +1,19 @@
 # Nesse exercício coletaremos dados de uma estudante, armazenaremos em um dicionário e imprimiremos na tela esses dados em um formato amigável.
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
+# 2. Armazene esses dados em um dicionário
+# 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcorridos, total de cursos realizados e (apenas) o primeiro e último curso.
+
 estudante = {}
 estudante['nome'] = input('Qual seu nome? ')
 estudante['ano_conheceu_linkedin'] = int(input('Quando conheceu o LinkedIn? '))
 estudante['ano_atual'] = int(input('Em que ano estamos? '))
-cursos_linkedIn = input('Quais cursos está realizando atualmente? ')
+cursos = input('Quais cursos você já fez no Linkedin Learning? (separe-os com vígula) ')
 
-# 2. Armazene esses dados em um dicionário
-estudante = {'nome','ano_conheceu_linkedin', 'ano_atual', 'cursos_linkedIn'}
-# 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcorridos, total de cursos realizados e (apenas) o primeiro e último curso.
-estudante['cursos_linkedIn'] = cursos_linkedIn.split(', ')
+estudante['cursos'] = cursos.split(', ')
+#estudante['cursos'] = cursos.split(', ')
+
 total_anos = estudante['ano_atual'] - estudante['ano_conheceu_linkedin']
-total_cursos = len(estudante['cursos_linkedIn'])
-print(f"Oi {estudante[0]}, visto que você conheceu o LinkedIn em {estudante['ano_conheceu_linkedin']}, há {estudante['ano_atual']- estudante['ano_conheceu_linkedin']} já realizou {total_cursos} cursos, sendo {estudante[cursos_linkedIn][0,-1]} o primeiro e último, respectivamente")
+
+total_cursos = len(estudante['cursos'])
+print(f"Oi {estudante['nome']}, visto que você conheceu o LinkedIn em {estudante['ano_conheceu_linkedin']}, há {total_anos} já realizou {total_cursos} cursos, sendo '{estudante['cursos'][0]}' o primeiro e '{estudante['cursos'][-1]}' o último.")
