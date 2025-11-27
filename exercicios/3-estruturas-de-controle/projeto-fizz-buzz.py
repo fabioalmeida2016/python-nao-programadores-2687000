91f1d0155c06f4d3d2f7f189a955b00b982d6354
fabioalmeida2016 <107158133+fabioalmeida2016@users.noreply.github.com>
2025-11-27 09:07:35 +0000

    caminhando

--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -5,4 +5,32 @@
 # 3.1 Caso o número seja divisível por 3, substitua-o por "Fizz"
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
+lista = []
 
+for i in range(16):
+  print(i)
+  lista.append(i)
+
+for i in lista:
+  if i%3 == 0 and i%5 != 0 and i < 10:
+      print(f'0{i} Fizz')
+  else:
+    print(f'{i} Fizz')
+
+  if i%5 == 0 and i%3 != 0 and i < 10:
+    print(f'0{i} Buzz')   
+  else: 
+    print(f'{i} Buzz')
+
+  if i%3 == 0 and i%5 == 0:
+    print(f'{i} FizzBuzz')
+  elif i%3 != 0 and i%5 != 0 and i < 10:
+      print(f'0{i} FizzBuzz')
+  else:
+    print(f'0{i} Buzz')  
+
+else:
+  if i < 10:
+    print(f'0{i} não é divisível por 3 nem 5')
+  else:
+    print(f'{i} não é divisível por 3 nem 5')
